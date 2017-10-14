6b59586eeba95b7d7600ef5f16b2392b7fa93460
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2017-10-14 14:51:21 -0400

    Add project.pbxproj

---
+++ TrackMyBudget/TrackMyBudget/View/CheckboxViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CheckboxViewCell.swift
+//  TrackMyBudget
+//
+//  Created by Prabhav Chawla on 10/14/17.
+//  Copyright © 2017 Prabhav Chawla. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
